e8d584b1133de07a338d3c7cf87ebc3337d7560a
Manu Sporny <msporny@digitalbazaar.com>
2026-04-12 17:50:33 -0400

    Fix static vector verification.

--- test/test-vectors.spec.js
+++ test/test-vectors.spec.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2023-2026 Digital Bazaar, Inc. All rights reserved.
  */
 import * as MldsaMultikey from '@digitalbazaar/mldsa-multikey';
-import {createSigner, cryptosuite} from '../lib/index.js';
+import {cryptosuite} from '../lib/index.js';
 import {DataIntegrityProof} from '@digitalbazaar/data-integrity';
 import {expect} from 'chai';
 import jsigs from 'jsonld-signatures';
@@ -26,44 +26,9 @@ function addTests({nistSecurityLevel, keyMaterial, signedFixture}) {
   let keyPair;
   before(async () => {
     keyPair = await MldsaMultikey.from(keyMaterial);
-    keyPair.controller = `did:key:${keyPair.publicKeyMultibase}`;
-    keyPair.id = `${keyPair.controller}#public-key`;
   });
 
-  it.skip(`should create ${label} proof`, async () => {
-    const unsigned = {...signedFixture};
-    delete unsigned.proof;
-
-    const signer = await createSigner({keyPair});
-    const date = new Date(signedFixture.proof.created);
-
-    let error;
-    let signed;
-    try {
-      signed = await jsigs.sign(unsigned, {
-        suite: new DataIntegrityProof({cryptosuite, signer, date}),
-        purpose: new AssertionProofPurpose(),
-        documentLoader
-      });
-    } catch(e) {
-      error = e;
-    }
-
-    expect(error).to.not.exist;
-    // ML-DSA is deterministic, so proof value must match exactly
-    expect(signed).to.deep.equal(signedFixture);
-
-    // ensure generated signed document verifies
-    const result = await jsigs.verify(signed, {
-      suite: new DataIntegrityProof({cryptosuite}),
-      purpose: new AssertionProofPurpose(),
-      documentLoader
-    });
-
-    expect(result.verified).to.be.true;
-  });
-
-  it.skip(`should verify ${label} signed fixture`, async () => {
+  it(`should verify ${label} signed fixture`, async () => {
     const result = await jsigs.verify(signedFixture, {
       suite: new DataIntegrityProof({cryptosuite}),
       purpose: new AssertionProofPurpose(),
